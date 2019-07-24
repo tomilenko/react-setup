21e5b13de726bc8a150b32a0cffbc35d61721631
Vadym Tomilenko <tomilenko.vadim@gmail.com>
2019-07-24 18:23:40 +0300

    Updated component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,18 +1,18 @@
-import * as React from "react";
-import "./Banner.scss";
+import * as React from 'react';
+import './Banner.scss';
 
 interface IProps {
-  name: string;
+    name: string;
 }
 
 export default class Banner extends React.Component<IProps> {
-  public render() {
-    return (
-      <div className="banner">
-        <span className="banner__text">
-          Hello {this.props.name}!
-        </span>
-      </div>
-    );
-  }
+    handleSmt = (id: number) => console.log(id);
+
+    public render(): any {
+        return (
+            <div className="banner">
+                <span className="banner__text">Hello {this.props.name}!</span>
+            </div>
+        );
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import "core-js";
-import "regenerator-runtime/runtime";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Banner from "./components/Banner/Banner";
-import "./styles/global.scss";
+import 'core-js';
+import 'regenerator-runtime/runtime';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Banner from './components/Banner/Banner';
+import './styles/global.scss';
 
 ReactDOM.render(
-  <div>
-    <Banner name="Vadym" />
-  </div>,
-  document.getElementById("app"),
+    <div>
+        <Banner name="Vadym" />
+    </div>,
+    document.getElementById('app')
 );
