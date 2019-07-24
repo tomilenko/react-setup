880c6e4cdcf9b98bc7cf990cf81dedf831745798
Vadym Tomilenko <tomilenko.vadim@gmail.com>
2019-07-24 18:24:19 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
 module.exports = {
-    mode: "development",
+    mode: 'development',
     watch: true,
-    entry: "./src/index.tsx",
+    entry: './src/index.tsx',
     output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.json'],
     },
-    devtool: "source-map",
+    devtool: 'source-map',
     module: {
-      rules: [
-        { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
-        { test: /\.tsx?$/, loader: "babel-loader" },
-        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
-    }
-  };
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            { test: /\.tsx?$/, loader: 'babel-loader' },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        ],
+    },
+    devServer: {
+        port: 9000,
+        hot: true,
+        liveReload: true,
+        open: true,
+        overlay: false,
+        progress: false,
+        stats: 'minimal',
+    },
+    plugins: [
+        new ErrorOverlayPlugin(),
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+    ],
+};
